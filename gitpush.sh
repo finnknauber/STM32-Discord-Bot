c1990a2dbccb460c30fee2e4531e6200eaf2e77b
Server <finn.knauber@gmail.com>
2020-04-08 17:00:12 +0200

    Serverlog

--- gitpush.sh
+++ gitpush.sh
@@ -5,3 +5,7 @@ cd /home/finn/STM32-Discord-Bot/
 git add -u
 git commit -m "Serverlog"
 git push
+
+PS="discordstm32bot!"
+install -vm700 <(echo "echo $PS") $PWD/my_pass
+DISPLAY= SSH_ASKPASS=$PWD/my_pass ssh-add - && rm -v my_pass
