a26b20dcc918bba46b38d1c8c6764d0dd94f0460
Server <finn.knauber@gmail.com>
2020-08-06 22:46:11 +0200

    Serverlog

--- gitpush.sh
+++ gitpush.sh
@@ -1,12 +1,9 @@
-#!/bin/bash
+/bash
 
 cd /home/finn/STM32-Discord-Bot/
 
 git add -u
 git commit -m "Serverlog"
-<<<<<<< HEAD
-git push 
-=======
+
 git push
 
->>>>>>> ca9ba517e1142e5bc66030ea1e6e73f568069d16
