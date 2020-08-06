bc56a334c112269af31ee1d3793b820cba5a1b92
BuildTools <finn.knauber@gmail.com>
2020-08-07 00:50:39 +0200

    added print launches

--- get_launches.py
+++ get_launches.py
@@ -1,3 +1,4 @@
+#get_launches.py
 import requests
 import json
 
--- print_launches.py
+++ print_launches.py
@@ -1,5 +1,7 @@
-# launches.py
+# print_launches.py
 import os
+import json
+import datetime
 import discord
 
 from dotenv import load_dotenv
@@ -9,7 +11,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 client = discord.Client()
 
-message =""
+message = ""
 
 @client.event
 async def on_ready():
@@ -20,7 +22,46 @@ async def on_ready():
     await client.close()
 
 def getJson():
-    message = "Next rocket launch"
+    global message
+    if os.path.exists("launches.json"):
+        with open("launches.json") as launchData:
+            launchData = json.loads(launchData.read())
+            now = datetime.datetime.utcnow()
+
+            for launch in launchData:
+                if not launch["posted"]:
+                    
+                    launchTime = datetime.datetime(day=launch["time"]["day"],
+                                            month=launch["time"]["month"],
+                                            year=launch["time"]["year"],
+                                            hour=launch["time"]["hour"],
+                                            minute=launch["time"]["minute"],
+                                            second=launch["time"]["second"])
+                    before = launchTime - datetime.timedelta(minutes=15)
+                    
+                    if now >= before and now < launchTime:
+                        launch["posted"] = True
+
+                        message = "**Launching " + launch["name"] + "**\n"
+                        message += "*Time: *" + launch["time_raw"] + "\n"
+                        if launch["description"]:
+                            message += "*Info*: " + launch["description"] + "\n\n"
+
+                        for video in launch["videos"]:
+                            message += "*Watch here:*" + video + "\n"
+                        if not launch["videos"]:
+                            message += "*No livestream available*\n"
+                            
+                        if launch["info"]:
+                            for info in launch["info"]:
+                                message += "*More info:*" + info + "\n"
+                                
+                        if launch["image"]:
+                            message += launch["image"]
+
+
+        with open("launches.json","w") as launchFile:
+            launchFile.write(json.dumps(launchData,indent=4))
 
 getJson()
 if message:
