9fb8b8675d4cd5fda9a5978e51d3ed664e70b6fc
BuildTools <finn.knauber@gmail.com>
2020-11-14 16:04:25 +0100

    added posted file

--- print_launches.py
+++ print_launches.py
@@ -32,61 +32,79 @@ def get_first_launch(launch_data):
     else:
         return {}
 
+def add_posted(id, net):
+    with open("posted.json") as oldData:
+        oldData = json.loads(oldData.read())
+
+    with open("posted.json", "w") as postedData:
+        oldData["posted"].append({id: net})
+        newData = json.dumps(oldData, skipkeys=True, indent=4)
+        postedData.seek(0)
+        postedData.write(newData)
+        postedData.truncate()
+
+def is_posted(id):
+    with open("posted.json") as postedData:
+        postedData = json.loads(postedData.read())
+        return id in postedData["posted"]
+
 
 def get_launch(launch_data):
     launch = ""
     if launch_data != "":
-        time = datetime.datetime.utcnow()
-        time = datetime.datetime(time.year, time.month, time.day, time.hour, time.minute)
-        if get_launches.format_time(launch_data["net"]) - datetime.timedelta(minutes=15) <= time:
-            current_launch_data = get_launches.get_launch_json()
-            if current_launch_data != get_launches.get_file_json():
-                current_launch_data = get_launches.write_launches(current_launch_data)
+        if not is_posted(launch_data["id"]):
+            time = datetime.datetime.utcnow()
+            time = datetime.datetime(time.year, time.month, time.day, time.hour, time.minute)
+            if get_launches.format_time(launch_data["net"]) - datetime.timedelta(minutes=15) <= time:
+                current_launch_data = get_launches.get_launch_json()
+                if current_launch_data != get_launches.get_file_json():
+                    current_launch_data = get_launches.write_launches(current_launch_data)
 
-            launch_data = get_first_launch(current_launch_data)
-            launch_string="**" + launch_data["name"] + "**"
+                launch_data = get_first_launch(current_launch_data)
+                launch_string="**" + launch_data["name"] + "**"
 
-            if "lsp_name" in launch_data:
-                launch_string+=" by ***" + launch_data["lsp_name"] + "***"
-            
-            launch_string+=" is launching in **15 Minutes**!\n"
+                if "lsp_name" in launch_data:
+                    launch_string+=" by ***" + launch_data["lsp_name"] + "***"
+                
+                launch_string+=" is launching in **15 Minutes**!\n"
 
-            if launch_data["launcher"]:
-                launch_string+="Using **" + launch_data["launcher"] + "**"
+                if launch_data["launcher"]:
+                    launch_string+="Using **" + launch_data["launcher"] + "**"
 
-                if "pad" in launch_data:
-                    launch_string+=" from **" + launch_data["pad"] + "**"
+                    if "pad" in launch_data:
+                        launch_string+=" from **" + launch_data["pad"] + "**"
+                        launch_string+=" in **" + launch_data["location"] + "**"
+
+                elif "pad" in launch_data:
+                    launch_string+="From **" + launch_data["pad"] + "**"
                     launch_string+=" in **" + launch_data["location"] + "**"
+                
+                if "landing" in launch_data:
+                    if launch_data["landing"]:
+                        launch_string+=", a landing will be attempted on/at **" + launch_data["landing"] + "**"
 
-            elif "pad" in launch_data:
-                launch_string+="From **" + launch_data["pad"] + "**"
-                launch_string+=" in **" + launch_data["location"] + "**"
-            
-            if "landing" in launch_data:
-                if launch_data["landing"]:
-                    launch_string+=", a landing will be attempted on/at **" + launch_data["landing"] + "**"
+                launch_string+=". \nThe current mission status is **" + launch_data["status"]["name"] + "**.\n"
 
-            launch_string+=". \nThe current mission status is **" + launch_data["status"]["name"] + "**.\n"
+                if "mission_type" in launch_data:
+                    launch_string+="The missions purpose is **" + launch_data["mission_type"] + "**."
 
-            if "mission_type" in launch_data:
-                launch_string+="The missions purpose is **" + launch_data["mission_type"] + "**."
+                if "orbit" in launch_data:
+                    if launch_data["orbit"]:
+                        launch_string+=" It is headed to **" + launch_data["orbit"] + "**."
 
-            if "orbit" in launch_data:
-                if launch_data["orbit"]:
-                    launch_string+=" It is headed to **" + launch_data["orbit"] + "**."
+                if "infographic" in launch_data:
+                    if launch_data["infographic"]:
+                        launch_string+="\n"+launch_data["infographic"]
+                    elif "image" in launch_data:
+                        if launch_data["image"]:
+                            launch_string+="\n"+launch_data["image"]
 
-            if "infographic" in launch_data:
-                if launch_data["infographic"]:
-                    launch_string+="\n"+launch_data["infographic"]
                 elif "image" in launch_data:
                     if launch_data["image"]:
                         launch_string+="\n"+launch_data["image"]
 
-            elif "image" in launch_data:
-                if launch_data["image"]:
-                    launch_string+="\n"+launch_data["image"]
-
-            launch+=launch_string + "\n\n"
+                launch+=launch_string + "\n\n"
+                add_posted(launch["id"], launch["net"])
 
     return launch
 
@@ -95,3 +113,4 @@ message = get_launch(get_first_launch(get_launches.get_file_json()))
 
 if message:
     client.run(TOKEN)
+
